45d07f9f7f6d3ec64c50c4cce1e31aed3eb33cbb
druvkotwani <druvkotwani12@gmail.com>
2023-10-25 14:17:24 +0530

    TrustedBy section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Hero from "./components/Hero";
+import TrustedBy from "./components/TrustedBy";
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <TrustedBy />
     </>
   )
 }
---
+++ src/components/TrustedBy.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TrustedBy = () => {
+    return (
+        <div className='border-t-[2px] border-[#212121] mt-16'>
+            <div className='inline-flex flex-col justify-center items-center gap-[14px] mt-16 px-8'>
+                <h1 className='text-gray-300 text-center font-[Outfit] font-semibold text-base'>
+                    Trusted by nearly 5000+ paying customers
+                </h1>
+                <img src='/assets/logos.png' />
+
+            </div>
+
+        </div>
+    )
+}
+
+export default TrustedBy
