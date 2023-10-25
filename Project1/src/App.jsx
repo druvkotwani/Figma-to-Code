5d9b65c723475ba903bb2373e78043d1e922421c
druvkotwani <druvkotwani12@gmail.com>
2023-10-25 21:28:58 +0530

    footer done

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -8,6 +8,7 @@ import Result2 from "./components/Result2";
 import ToggleSection from "./components/ToggleSection";
 import Testimonial from "./components/Testimonial";
 import CardSection from "./components/CardSection";
+import Footer from "./Footer";
 export default function App() {
   return (
     <div className="overflow-hidden">
@@ -28,6 +29,18 @@ export default function App() {
       <Testimonial />
       <CardSection />
 
+      <div className="bg-[linear-gradient(90deg,_#2b59ff_0%,#bb2bff_100%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-col justify-center gap-5 w-full items-center p-8">
+        <div className="text-3xl font-['Outfit'] font-semibold text-[#e0e0e0] w-full">
+          It will help you improve your writing
+          <br />& bring ideas more c learly.
+        </div>
+        <div className="border-solid border-[#676767] bg-[#f5f5f5] flex flex-col justify-center w-3/5 h-12 shrink-0 items-center border">
+          <div className="text-sm font-['Inter'] font-medium text-[#474747]">
+            Start 14 days free trial
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
---
+++ Project1/src/Footer.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="flex flex-col justify-center gap-24 w-full py-16 px-6">
+            <div className="flex flex-row justify-between items-start mx-1">
+                <div className="flex flex-col justify-between w-16 shrink-0 h-40 items-start">
+                    <div className="flex flex-col gap-4 w-16 h-12 shrink-0 items-start">
+                        <div className="text-sm font-['Inter'] font-bold text-[#dcdcdc]">
+                            Company
+                        </div>
+                        <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                            About
+                        </div>
+                    </div>
+                    <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                        Feature
+                    </div>
+                    <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                        Works
+                    </div>
+                    <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                        Career
+                    </div>
+                </div>
+                <div className="flex flex-col justify-between h-40 items-start">
+                    <div className="flex flex-col gap-4 w-32 h-12 shrink-0 items-start">
+                        <div className="text-sm font-['Inter'] font-bold text-[#dcdcdc]">
+                            Help
+                        </div>
+                        <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                            Customer support
+                        </div>
+                    </div>
+                    <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                        Delivery details
+                    </div>
+                    <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                        Terms & condition
+                    </div>
+                    <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                        Pravacy policy
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-row justify-between items-start mx-1">
+                <div className="flex flex-col justify-between w-32 shrink-0 h-40 items-start">
+                    <div className="flex flex-col gap-4 w-16 h-12 shrink-0 items-start">
+                        <div className="text-sm font-['Inter'] font-bold text-[#dcdcdc]">
+                            Resources
+                        </div>
+                        <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                            Free
+                        </div>
+                    </div>
+                    <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                        Development
+                    </div>
+                    <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                        How to -blog
+                    </div>
+                    <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                        Youtube
+                    </div>
+                </div>
+                <div className="flex flex-col justify-between h-40 items-start">
+                    <div className="flex flex-col gap-4 w-32 h-12 shrink-0 items-start">
+                        <div className="text-sm font-['Inter'] font-bold text-[#dcdcdc]">
+                            Links
+                        </div>
+                        <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                            Free ebooks
+                        </div>
+                    </div>
+                    <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                        Development details
+                    </div>
+                    <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                        How to blog
+                    </div>
+                    <div className="text-sm font-['Inter'] font-medium text-[#bebebe]">
+                        Youtube playlist
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Footer
