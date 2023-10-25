d1bddb8d507c5ebdb11ea44114911031a5865fa9
druvkotwani <druvkotwani12@gmail.com>
2023-10-25 14:26:18 +0530

    Results

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Navbar from "./components/Navbar";
 import "./App.css";
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
+import Results from "./components/Results";
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <TrustedBy />
+      <Results />
     </>
   )
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Results = () => {
+    return (
+        <div className='mt-16 pl-5 pr-8'>
+            <div className="relative flex flex-col justify-end items-end ">
+                <img
+                    src="https://file.rendit.io/n/BbbYofMFGMK97timZsum.svg"
+                    className="w-[268px] h-[297px] bg-[url(https://file.rendit.io/n/YSX5R7siCvNLr0SFrHg7.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat absolute top-0 left-0"
+                />
+                <img
+                    src="https://file.rendit.io/n/VNE5sn287CPPqtAzrJGI.svg"
+                    className="relative pt-[95px]"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Results
