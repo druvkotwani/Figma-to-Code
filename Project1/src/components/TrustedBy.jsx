fad068c6f9874b5c5d71939ac09403453181d07f
druvkotwani <druvkotwani12@gmail.com>
2023-10-26 00:12:23 +0530

    doing results section

--- Project1/src/components/TrustedBy.jsx
+++ Project1/src/components/TrustedBy.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const TrustedBy = () => {
     return (
         <div className='border-t-[2px] border-[#212121] mt-16'>
-            <div className='inline-flex flex-col justify-center items-center gap-[14px] mt-16 px-8'>
-                <h1 className='text-gray-300 text-center font-[Outfit] font-semibold text-base'>
+            <div className='inline-flex flex-col justify-center items-center gap-[14px] mt-16 px-8 md:px-auto w-full '>
+                <h1 className='text-gray-300 text-center font-[Outfit] md:text-xl    font-semibold text-base'>
                     Trusted by nearly 5000+ paying customers
                 </h1>
-                <img src='/assets/logos.png' />
+                <img className="md:w-1/2  md:pt-4"
+                    src='/assets/logos.png' />
 
             </div>
 
