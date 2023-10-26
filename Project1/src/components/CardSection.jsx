b3a888bad74ef9a6b94782b8e6e4afd48a43aae5
druvkotwani <druvkotwani12@gmail.com>
2023-10-26 13:45:47 +0530

    cards responsive

--- Project1/src/components/CardSection.jsx
+++ Project1/src/components/CardSection.jsx
@@ -4,12 +4,11 @@ import Describe2 from './Describe2'
 
 const CardSection = () => {
     return (
-        <div className='flex flex-col justify-center items-center gap-[45.753px]'>
+        <div className='flex flex-col my-16 justify-center items-center gap-[45.753px]'>
 
-            <div className='pt-16 px-8'>
-                <Describe2 text='Make the wise decision for your business' />
-            </div>
-            <div className='pt-4 pb-8 py-8 flex flex-col justify-center items-center gap-[33.552px]'>
+            <Describe2 text='Make the wise decision for your business' />
+
+            <div className='pt-4 pb-8 py-8 flex md:flex-row flex-col justify-center items-center gap-[33.552px]'>
                 <Card plan="Basic" price="$29" />
                 <Card plan="Standard" price="$149" color='bg-[linear-gradient(90deg,_#2b59ff_0%,#bb2bff_100%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat' />
                 <Card plan="Premium" price="$299" />
--- Project1/src/components/Describe2.jsx
+++ Project1/src/components/Describe2.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Describe2 = ({ text }) => {
     return (
         <div className="flex flex-col gap-4 w-full items-center">
-            <div className="text-center text-3xl font-['Outfit'] font-semibold text-[#e0e0e0] w-full">
+            <div className="text-center md:text-4xl text-3xl font-['Outfit'] font-semibold text-[#e0e0e0] ">
                 {text}
             </div>
-            <div className="text-center text-xs font-['Inter'] font-medium text-[#b7b7b7] self-start w-full">
+            <div className="text-center md:text-[16px] text-xs font-['Inter'] font-medium text-[#b7b7b7] self-start md:w-[40%] leading-snug mx-auto w-full">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna{" "}
+                tempor incididunt ut labore et dolore magna
             </div>
         </div>
     )
