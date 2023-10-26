d415af668068ac8b71907bf4a04be81b12f601ed
druvkotwani <druvkotwani12@gmail.com>
2023-10-26 14:05:54 +0530

    purple sectoin

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -29,16 +29,17 @@ export default function App() {
       <Testimonial />
       <CardSection />
 
-      <div className="bg-[linear-gradient(90deg,_#2b59ff_0%,#bb2bff_100%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-col justify-center gap-5 w-full items-center p-8">
-        <div className="text-3xl font-['Outfit'] font-semibold text-[#e0e0e0] w-full">
+
+      <div className="md:w-[768px] lg:w-[1022px] mx-auto bg-[linear-gradient(90deg,_#2b59ff_0%,#bb2bff_100%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat md:flex-row flex flex-col justify-center gap-[102px] w-full items-center p-8">
+        <p className="text-3xl font-['Outfit'] font-semibold text-[#e0e0e0] w-[582px]">
           It will help you improve your writing
-          <br />& bring ideas more c learly.
-        </div>
-        <div className="border-solid border-[#676767] bg-[#f5f5f5] flex flex-col justify-center w-3/5 h-12 shrink-0 items-center border">
-          <div className="text-sm font-['Inter'] font-medium text-[#474747]">
+          <br />& bring ideas more clearly.
+        </p>
+        <button className="border-solid border-[#676767] bg-[#f5f5f5] flex flex-col justify-center md:w-1/5 w-3/5 h-12 shrink-0 items-center border">
+          <p className="text-sm font-['Inter'] font-medium text-[#474747]">
             Start 14 days free trial
-          </div>
-        </div>
+          </p>
+        </button>
       </div>
       <Footer />
     </div>
--- Project1/src/components/Results.jsx
+++ Project1/src/components/Results.jsx
@@ -6,11 +6,11 @@ const Results = () => {
     return (
         <div className='inline-flex flex-col justify-center items-center gap-[40px] '>
 
-            <div className=' w-[50%]  mt-16 pl-5 pr-8 '>
+            <div className=' w-full mt-16 pl-5 pr-8'>
                 <div className="relative flex flex-col justify-end items-end ">
                     <img
                         src="https://file.rendit.io/n/BbbYofMFGMK97timZsum.svg"
-                        className="w-[268px] md:w-[375px] md:h-[415px] h-[297px] bg-[url(https://file.rendit.io/n/YSX5R7siCvNLr0SFrHg7.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat absolute top-0 left-0"
+                        className="w-[268px] h-[297px] bg-[url(https://file.rendit.io/n/YSX5R7siCvNLr0SFrHg7.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat absolute top-0 left-0"
                     />
                     <img
                         src="https://file.rendit.io/n/VNE5sn287CPPqtAzrJGI.svg"
