ea490c7ab7ff2266adfc14ebdafac7037ac892de
druvkotwani <druvkotwani12@gmail.com>
2023-10-26 16:52:48 +0530

    resultws done

--- Project1/src/components/Result2.jsx
+++ Project1/src/components/Result2.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
 import Describe from './Describe'
 import Button from './Button'
 const Result2 = () => {
     return (
-        <div className='inline-flex flex-col justify-center items-center gap-[40px] '>
+        <div className='md:grid grid-cols-2 inline-flex flex-col justify-center items-center  gap-[40px] md:w-[90%] md:ml-auto  md:mt-16'>
 
-            <div className='pl-5 mt-16 pr-8'>
+            <div className='pl-5 mt-16 pr-8 md:w-[60%] md:mr-auto md:order-1 '>
                 <div className="relative flex flex-col justify-end items-end ">
                     <img
                         src="https://file.rendit.io/n/8hP4XrPpIgRq7iwzNrdq.svg"
-                        className="bg-[url(https://file.rendit.io/n/Yrwzp4rlxYaaCL0zau36.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat"
+                        className="bg-[url(https://file.rendit.io/n/Yrwzp4rlxYaaCL0zau36.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat "
                     />
                 </div>
             </div>
 
-            <div className='pl-8 pr-16 '>
-                <Describe text='Write what you want to
-convey through clear,&
-authentic writing' />
+            <div className='pl-8 pr-16 md:w-[90%] md:ml-auto md:mt-16 '>
+                <Describe text='Write what you want to convey through clear,& authentic writing' />
                 <div className='pt-8'>
                     <Button text='Start 14 days free trial' />
                 </div>
--- Project1/src/components/Results.jsx
+++ Project1/src/components/Results.jsx
@@ -4,13 +4,13 @@ import Button from './Button'
 
 const Results = () => {
     return (
-        <div className='inline-flex flex-col justify-center items-center gap-[40px] '>
+        <div className='inline-flex md:grid grid-cols-2 flex-col justify-center items-center md:w-[90%] md:pl-8 md:mx-auto gap-[40px] '>
 
-            <div className=' w-full mt-16 pl-5 pr-8'>
+            <div className=' w-full mt-16 pl-5 pr-8 md:w-[60%] md:mx-auto'>
                 <div className="relative flex flex-col justify-end items-end ">
                     <img
                         src="https://file.rendit.io/n/BbbYofMFGMK97timZsum.svg"
-                        className="w-[268px] h-[297px] bg-[url(https://file.rendit.io/n/YSX5R7siCvNLr0SFrHg7.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat absolute top-0 left-0"
+                        className="w-[268px] h-[297px]  md:w-[301px] md:h-[335px]  bg-[url(https://file.rendit.io/n/YSX5R7siCvNLr0SFrHg7.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat absolute top-0 left-0"
                     />
                     <img
                         src="https://file.rendit.io/n/VNE5sn287CPPqtAzrJGI.svg"
@@ -19,7 +19,7 @@ const Results = () => {
                 </div>
             </div>
 
-            <div className='pl-8 pr-16 '>
+            <div className='pl-8 pr-16 md:w-[90%] mx-auto  md:mt-16 md:order-1'>
                 <Describe text='Create content efficiently
                 and quickly with great AI
                 writing tools'/>
